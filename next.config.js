913c46d21e85e246155d84c7f22b9b96eec0b2a7
Joao Pedro <jpedroteixeiraphrom@gmail.com>
2023-05-20 20:23:37 -0300

    hotfix images

--- next.config.js
+++ next.config.js
@@ -2,6 +2,21 @@
 const nextConfig = {
   trailingSlash: true,
   output: "export",
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(png|jpg|gif)$/i,
+      use: [
+        {
+          loader: "url-loader",
+          options: {
+            limit: 8192,
+          },
+        },
+      ],
+    });
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
